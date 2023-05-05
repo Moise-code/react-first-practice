6c0b4013cc7b740c54c9a7ae720a7cbe18fb70be
Moise-code <moisewowe@gmail.com>
2023-05-05 11:05:12 +0200

    adding styles to the components

--- first-practice/src/UserInput/UserInput.js
+++ first-practice/src/UserInput/UserInput.js
@@ -1,9 +1,8 @@
 import React from "react";
-
-
+import './UserInput.css';
 const userInput = (props) =>{
   return(
-    <div>
+    <div className="UserInput">
       <input type="text" onChange={props.changed} value={props.currentName} />
     </div>
   )
