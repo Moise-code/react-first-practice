7a124fbdc1529d7b7808027becde0838e7696d9d
Moise-code <moisewowe@gmail.com>
2023-05-05 09:47:39 +0200

    adding the new components

---
+++ first-practice/src/UserInput/UserInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const userInput = () =>{
+  return(
+    <div>
+      <input type="text" />
+    </div>
+  )
+}
+
+export default userInput;
---
+++ first-practice/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = (props) =>{
+  return(
+    <div>
+      <p>This is amaizing.</p>
+      <p>Hello how are you doing.</p>
+    </div>
+
+  )
+}
+export default userOutput;
