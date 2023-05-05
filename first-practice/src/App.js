ba903cc70177e439c90cc0626e1ccb82136b206d
Moise-code <moisewowe@gmail.com>
2023-05-05 09:59:58 +0200

    adding the user name using props

--- first-practice/src/App.js
+++ first-practice/src/App.js
@@ -17,11 +17,13 @@ class App extends Component {
           <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
           <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+          <li> Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ol>
 
         <UserInput />
-        <UserOutput />
+        <UserOutput userName = "Byiringiro" />
+        <UserInput userName="Bugingo" />
+        <UserOutput userName="Kirenga" />
       </div>
     );
   }
