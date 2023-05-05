1f7e571d5f24b8f085a0db2e71d854560c6b2b91
Moise-code <moisewowe@gmail.com>
2023-05-05 11:13:39 +0200

    adding the refactoring codes

--- first-practice/src/UserOutput/UserOutput.js
+++ first-practice/src/UserOutput/UserOutput.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './UserOutput.css'
 
 const userOutput = (props) =>{
   return(
     <div>
-      <p>This is amaizing. your name is {props.userName}</p>
-      <p>Hello how are you doing. your last name is {props.userName}</p>
+      <p>This is amaizing. your name is <span className="input">{props.userName}</span> </p>
+      <p>Hello how are you doing. your last name is <span className="input">{props.userName}</span></p>
     </div>
 
   )
